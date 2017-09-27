058db07bc0fe6be400b58b247f402791fe802930
Carsten Zimmermann <cz@aegisnet.de>
2017-09-27 23:33:38 +0200

    Add global (but thread-safe) source collection

--- lib/sensorr.rb
+++ lib/sensorr.rb
@@ -3,4 +3,28 @@ require "sensorr/factory"
 require "thread"
 
 module Sensorr
+  @sources = []
+  @_mtx = Mutex.new
+
+  class << self
+    def sources
+      @_mtx.synchronize do
+        @sources ||= []
+      end
+    end
+
+    def push(source)
+      @_mtx.synchronize do
+        @sources.push source
+      end
+    end
+    alias add push
+    alias << push
+
+    def pop
+      @_mtx.synchronize do
+        @sources.pop source
+      end
+    end
+  end
 end
--- test/sensorr_test.rb
+++ test/sensorr_test.rb
@@ -5,7 +5,20 @@ class SensorrTest < Minitest::Test
     refute_nil ::Sensorr::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_that_it_responds_to_sources
+    assert ::Sensorr.respond_to?(:sources)
+    assert ::Sensorr.sources.kind_of?(Array)
   end
+
+  def test_that_it_allows_sources_manipulation
+    # Add a source
+    source = Object.new
+    Sensorr.push(source)
+    assert_equal [source], Sensorr.sources
+
+    # Remove the source
+    assert_equal source, Sensorr.sources.pop
+    assert_equal [], Sensorr.sources
+  end
+
 end
