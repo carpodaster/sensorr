995af7553b2fd452b76147b0f30316ff8e8c4297
Carsten Zimmermann <cz@aegisnet.de>
2017-09-27 23:43:23 +0200

    Populate sources collection with sensors data

--- lib/sensorr.rb
+++ lib/sensorr.rb
@@ -6,6 +6,8 @@ module Sensorr
   @sources = []
   @_mtx = Mutex.new
 
+  SENSORS_BIN="/usr/bin/sensors"
+
   class << self
     def sources
       @_mtx.synchronize do
@@ -26,5 +28,12 @@ module Sensorr
         @sources.pop source
       end
     end
+
+    def update!
+      @_mtx.synchronize do
+        @sources = Sensorr::Factory.from_raw_sensors_output(`#{SENSORS_BIN} -u`)
+      end
+    end
   end
+
 end
